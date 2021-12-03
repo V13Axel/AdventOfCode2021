2d3202260d3e43d47d2d7bd3c3eb3df62e26c8b3
V13Axel <alex@stechstudio.com>
2021-12-02 21:34:51 -0500

    Prepare boilerplate files and such for the rest of AoC days

--- main.go
+++ main.go
@@ -12,6 +12,28 @@ import (
 var StubStorage = map[string]interface{}{
 	"Day1": Day1,
 	"Day2": Day2,
+	// "Day3": Day3,
+	// "Day4": Day4,
+	// "Day5": Day5,
+	// "Day6": Day6,
+	// "Day7": Day7,
+	// "Day8": Day8,
+	// "Day9": Day9,
+	// "Day10": Day10,
+	// "Day11": Day11,
+	// "Day13": Day13,
+	// "Day14": Day14,
+	// "Day15": Day15,
+	// "Day16": Day16,
+	// "Day17": Day17,
+	// "Day18": Day18,
+	// "Day19": Day19,
+	// "Day20": Day20,
+	// "Day21": Day21,
+	// "Day22": Day22,
+	// "Day23": Day23,
+	// "Day24": Day24,
+	// "Day25": Day25,
 }
 
 func main() {
